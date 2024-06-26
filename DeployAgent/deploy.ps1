8bbd4bf9533fe51f0f5a575ec0c46c996913631e
Mark Hobart <30318189+yerttle@users.noreply.github.com>
2024-06-26 14:58:44 +0000

    Added Fail safe

--- DeployAgent/deploy.ps1
+++ DeployAgent/deploy.ps1
@@ -1,13 +1,16 @@
 # ********************************************
-# Make sure you double check this OrganisationID changing organisations is not trivial.
-# 
 # If you are deploying via intune I would recommend using our intune integration feature.
+# 
+# How to use:
+# Replace YOUR_ORGANISATION_ID with the organisationID from the correct organisation in the portal. Note changing this after install is not trvial.
+# Set $haveSetOrgId to $True
 # ********************************************
 
 $organisationId = "YOUR_ORGANISATION_ID"
+$haveSetOrgId = $False
 
 $version = (Get-ItemProperty -Path "HKLM:\SOFTWARE\RoboShadowLtd\Rubicon\Agent" -Name "Version" -ErrorAction SilentlyContinue).$valueName
 
-if (-not $version -or [int]($version -split '\.')[0] -lt 4) {
+if ($haveSetOrgId -and (-not $version -or [int]($version -split '\.')[0] -lt 4)) {
   Start-Process "C:\Windows\System32\msiexec.exe" -ArgumentList "/i https://cdn.roboshadow.com/GetAgent/RoboShadowAgent-x64.msi /qb /norestart ORGANISATION_ID=$organisationId" -Wait
 }
